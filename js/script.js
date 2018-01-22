a11cbe00c3342c8632d1d87c40e1dad8560e552a
Palette25 <30751070+Palette25@users.noreply.github.com>
2018-01-22 18:42:17 +0800

    upload page modified

---
+++ js/script.js
@@ -0,0 +1,15 @@
+/* Created by Palette at 2018/01/22 */
+
+
+window.onload = function(){
+	$('#btn').click = onClick;
+}
+
+function onClick(){
+	var temp = $('pass').value;
+	if(temp==='1377278217'){
+
+	}else {
+		alert('You have enter the wrong password!!\nPlease leave this page if you are not the root!!');
+	}
+}
